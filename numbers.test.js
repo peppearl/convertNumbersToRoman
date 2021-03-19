4f6626acab347080c8b8e33fc18e280cd0a5e6e9
Gabrielle Geroudet <gabrielle.geroudet@gmail.com>
2021-03-19 17:07:44 +0100

    test

--- numbers.test.js
+++ numbers.test.js
@@ -2,7 +2,11 @@ require('./numbers');
 
 test('3999  est égal à MMMCMXCIX', () => {
     expect(convertToRoman(3999)).toBe('MMMCMXCIX');
+    expect(convertToRoman(1245)).toBe('MCCXLV');
+    expect(convertToRoman(48)).toBe('XLVIII');
 });
 test('MMMCMXCIX  est égal à 3999', () => {
     expect(convertToNumber('MMMCMXCIX')).toBe(3999);
+    expect(convertToRoman('MCCXLV')).toBe(1245);
+    expect(convertToRoman('XLVIII')).toBe(48);
 });
